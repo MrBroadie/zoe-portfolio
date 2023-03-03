070b8f1aa87decbd966b99f1829abfa2c0907243
Tom Broad <thomas.broad@ecs.co.uk>
2023-03-03 18:30:22 +0000

    moved hash router to index

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Footer from "./components/Footer";
@@ -9,14 +9,12 @@ import Portfolio from "./routes/Portfolio";
 function App() {
   return (
     <>
-      <HashRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="portfolio" element={<Portfolio />} />
-        </Routes>
-        <Footer />
-      </HashRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="portfolio" element={<Portfolio />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
